1697f9b02566610f69698e097e498e9a95f87e7e
swhitmore <steve@swhitmore.com>
2018-11-28 19:22:55 -0600

    conditional path value for santa banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Santas Appeal Form';
-  bannerImg = '/assets/images/SantaBanner.gif';
+  bannerImg = environment.production ? '/santas-appeal-form//assets/images/SantaBanner.gif' : '/assets/images/SantaBanner.gif';
 }
